2a776127b18bfa87490782151ebe09fca37b2e68
cameronmi <y7tsai+cameronmi@gmail.com>
2016-10-28 15:49:02 -0700

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,14 +1,17 @@
 (function($) {
 $(document).ready(function(){
    
-   // Variable to hold request
-   var request;
+  // Variable to hold request
+  var request;
+  var data;
+  var ids=['sn', 'in', 'ln'];
+  var columns=['scene', 'item', 'line'];
+  // Bind to the submit event of our form
+  $("#form").submit(function(event){
 
-   // Bind to the submit event of our form
-   $("#form").submit(function(event){
-
-     // Prevent default posting of form - put here to work in case of errors
-     event.preventDefault();
+  // Prevent default posting of form - put here to work in case of errors
+  event.preventDefault();
+  ga('send', 'event', 'Button', 'Click', 'generate');
 
      //Abort any pending request
      if (request) {
@@ -20,8 +23,23 @@ $(document).ready(function(){
      // Let's select and cache all the fields
      var $inputs = $form.find("input, select, button, textarea");
 
-     // Serialize the data in the form
-     var serializedData = $form.serialize();
+     var PTTID = $("#id").val();
+     var formdata = {
+       "id": PTTID
+     };
+     var results = [];
+     var inputs = _.map(ids, function(id, i) {
+       var $select = $("#"+id);
+       var num = $select.val();
+       var options = _.join(_.slice(array, _.shuffle(data[i]), 0, num));
+       formdata[columns[i]] = options;
+       $("#"+columns[i]).html(options);
+       results.push("(" + options + ")");
+       return num;
+     });
+     formdata["input"] = _.join(inputs);
+     ga('send', 'event', PTTID, _.join(inputs), _.join(results));
+    
 
      // Let's disable the inputs for the duration of the Ajax request.
      // Note: we disable elements AFTER the form data has been serialized.
@@ -32,7 +50,7 @@ $(document).ready(function(){
      request = $.ajax({
          url: "https://script.google.com/macros/s/AKfycbxjJ73uGfOjfeJqi08U75gW7-mB3TMF5Wu5iezg-cNZiH_z_H4/exec",
          type: "post",
-         data: serializedData
+         data: JSON.stringify(formdata);
      });
 
      // Callback handler that will be called on success
@@ -83,10 +101,14 @@ $(document).ready(function(){
     xhr.send();
     xhr.onreadystatechange = function () {
       if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        var data = xhr.response;
-        first = data[0];
-        second = data[1];
-        third = data[2];
+        data = xhr.response;
+        _.each(ids, function(v, i) {
+           var $select = $("#"+v);
+           _.each(data[i], function(v, i) {
+              $select.append($('<option value=' + (i+1) + '>' + (i+1) +'</option>'));
+           });
+        });
+        
 //        document.getElementById('generate-button').removeAttribute('disabled');
       }
     };
